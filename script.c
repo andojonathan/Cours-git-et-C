c11d7cfd281ad7669cc3b4da1bb44037095c013f
andoJo <ando.jo.mada@gmail.com>
2019-07-17 11:44:17 +0300

    Ajout de exectarget et de fonctionalité exec

--- script.c
+++ script.c
@@ -9,6 +9,13 @@ int main(void){
 	processID = fork();	
 	if (processID ==0){
 		printf("Coucou c'est moi le fils");
+		char *args[]={"./exectarget",NULL}; 
+        execvp(args[0],args); 
+      
+        /*All statements are ignored after execvp() call as this whole  
+        process(execDemo.c) is replaced by another process (EXEC.c) 
+        */
+        printf("Ending-----");
 }
 }
 
